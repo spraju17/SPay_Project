9e6a90aa9f6eb7c88654f3e7bf8ab53174fe4b26
Sarath <Sarath@DESKTOP-VIBKR6L>
2021-05-23 15:45:29 +0530

    started working

---
+++ spay/src/main/java/com/spraju/spay/SpayApplication.java
@@ -0,0 +1,13 @@
+package com.spraju.spay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpayApplication.class, args);
+	}
+
+}
---
+++ spay/src/test/java/com/spraju/spay/SpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spraju.spay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
